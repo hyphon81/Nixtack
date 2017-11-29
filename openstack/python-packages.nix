397a5b6792afe7a6218632800913ad5899376843
hyphon81 <zero812n@gmail.com>
2017-11-30 00:13:10 +0900

    Fix for NixOS 17.03

--- openstack/python-packages.nix
+++ openstack/python-packages.nix
@@ -3678,7 +3678,7 @@ with python2Packages;
       whoosh
       imagesize
       modpacks.requests
-      sphinxcontrib-websupport
+      modpacks.sphinxcontrib-websupport
       typing
     ];
 
@@ -4434,6 +4434,21 @@ with python2Packages;
     };
 
     # No tests included
+    doCheck = false;
+  };
+
+  sphinxcontrib-websupport = buildPythonPackage rec {
+    pname = "sphinxcontrib-websupport";
+    version = "1.0.1";
+    name = "${pname}-${version}";
+
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "7a85961326aa3a400cd4ad3c816d70ed6f7c740acd7ce5d78cd0a67825072eb9";
+    };
+
+    propagatedBuildInputs = [ six ];
+
     doCheck = false;
   };
 
